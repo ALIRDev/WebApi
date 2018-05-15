58c91ccfb8c525faff32fba59d2d7bbc9f23f540
Andrea <andrea.zago@medas-solutions.it>
2018-05-15 12:21:17 +0200

    fs.statSync per orario del dump

--- api/alirdb.js
+++ api/alirdb.js
@@ -1,17 +1,13 @@
 const fs             = require("file-system");
 const jsonQuery      = require('json-query');
 const request        = require('request');
-const cron           = require('node-cron');
-const https          = require('https');
 
 // Path file-system
-const playersJson           = "/alirdb/player.json";
+const playersJson           = "/webapi/today/player.json";
 const gangsJson             = "/alirdb/gangs.json";
 const vehiclesJson          = "/alirdb/vehicles.json";
 const wantedJson            = "/alirdb/wanted.json";
 const usersJson             = "/alirdb/users.json";
-const generalsForumFeed     = "/alirdb/forumGeneralFeed.json";
-const adviceForumFeed       = "/alirdb/forumAdviceFeed.json";
 const fileEncrypt           = "utf8";
 
 // TODO: HTTPS Request
@@ -55,11 +51,19 @@ module.exports = function (app) {
      *   @param: req = Url della richiesta
      *   @param: res = Risposta alla richiesta
      *   @return: Array di oggetti
-     *   @example: http://192.168.30.77:8000/status --> [{"ok":"Sistema online"}]
+     *   @example: http://192.168.30.77:8000/status --> {"Stato":"Online","Ultimo aggiornamento":"2018-5-15 11:56"}
      */
 
     app.get('/status', (req, res, next) => {
-        res.send({"ok": "Sistema online"});
+        let stats = fs.statSync(playersJson);
+        let UTCTime = stats.ctime;
+        let options = { timeZone: "Europe/Rome", day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'};
+        let formatter = new Intl.DateTimeFormat([], options);
+
+        res.send({
+           "Stato" : "Online",
+           "Ultimo aggiornamento" : formatter.format(new Date(UTCTime))
+        })
     });
 
     /**
